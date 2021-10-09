5359e205e6c984effb94465f95716a75292120c2
a.bogdan <a.bogdan@quantower.com>
2021-10-09 22:02:12 +0300

    GDIBasedPlugin

---
+++ Plugins/GDIBasedPlugin/GDIBasedPlugin/GDIBasedPlugin.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Drawing;
+using TradingPlatform.BusinessLayer;
+using TradingPlatform.PresentationLayer.Plugins;
+
+namespace GDIBasedPlugin
+{
+    public class GDIBasedPlugin : Plugin
+    {
+        private GDIRenderer gdiRenderer;
+
+        /// <summary>
+        /// Plugin meta information
+        /// </summary>
+        public static PluginInfo GetInfo()
+        {
+            var windowParameters = NativeWindowParameters.Panel;
+            windowParameters.AllowDrop = true;
+            windowParameters.BrowserUsageType = BrowserUsageType.None;
+
+            return new PluginInfo()
+            {
+                Name = "GDIBasedPlugin",
+                Title = "GDIBasedPlugin",
+                Group = PluginGroup.Misc,
+                ShortName = "GDI",
+                SortIndex = 35,
+                AllowSettings = true,
+                WindowParameters = windowParameters,
+                CustomProperties = new Dictionary<string, object>()
+                {
+                    {PluginInfo.Const.ALLOW_MANUAL_CREATION, true }
+                }
+            };
+        }
+
+        /// <summary>
+        /// Default plugin size - can be an absolute value or a multiple of UnitSize (UnitSize depends on the monitor resolution)
+        /// </summary>
+        public override Size DefaultSize => new Size(this.UnitSize.Width * 1, this.UnitSize.Height * 2);
+
+        /// <summary>
+        /// Initialize called once on plugin creation
+        /// </summary>
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            //
+            this.gdiRenderer = new GDIRenderer(this.Window.CreateRenderingControl("GDIRenderer"));
+        }
+
+        /// <summary>
+        /// Populate called on plugin creation and each time when any connection get connected/disconnected
+        /// </summary>
+        public override void Populate(PluginParameters args = null)
+        {
+            base.Populate(args);
+
+            // Redraw renderer
+            this.gdiRenderer.RedrawBufferedGraphic();
+        }
+
+        public override void Dispose()
+        {
+            // 
+            if (this.gdiRenderer != null)
+            {
+                this.gdiRenderer.Dispose();
+                this.gdiRenderer = null;
+            }
+
+            base.Dispose();
+        }
+
+        public override IList<SettingItem> Settings 
+        {
+            get
+            {
+                var result = base.Settings;
+
+                // Here you can specify customf settings for your plugin
+                result.Add(new SettingItemColor("Color", this.gdiRenderer.Color));
+
+                return result;
+            }
+            set
+            {
+                base.Settings = value;
+
+                // Apply custom settings
+                if (value.GetItemByPath("Color") is SettingItemColor color)
+                {
+                    this.gdiRenderer.Color = (Color)color.Value;
+                    this.gdiRenderer.RedrawBufferedGraphic();
+                }
+            }
+        }
+
+        protected override void OnLayoutUpdated()
+        {
+            base.OnLayoutUpdated();
+
+            if (this.gdiRenderer != null)
+                this.gdiRenderer.Layout.Margin = this.NonClientMargin;
+        }
+    }
+}
---
+++ Plugins/GDIBasedPlugin/GDIBasedPlugin/GDIRenderer.cs
@@ -0,0 +1,109 @@
+﻿using System.Drawing;
+using TradingPlatform.BusinessLayer;
+using TradingPlatform.BusinessLayer.Native;
+using TradingPlatform.PresentationLayer.Plugins;
+using TradingPlatform.PresentationLayer.Renderers;
+using System.Linq;
+
+namespace GDIBasedPlugin
+{
+    public class GDIRenderer : Renderer
+    {
+        #region Proeprties
+
+        private BufferedGraphic bufferedGraphic;
+
+        public Color Color { get; set; }
+
+        #endregion
+
+        public GDIRenderer(IRenderingNativeControl native)
+           : base(native)
+        {
+            // Subscribe to mouse events
+            native.MouseDownNative += this.OnMouseDown;
+            native.MouseUpNative += this.OnMouseUp;
+            native.MouseMoveNative += this.OnMouseMove;
+
+            //
+            this.Color = Color.Black;
+            this.bufferedGraphic = new BufferedGraphic(this.Draw, this.Refresh, native.DisposeImage, native.IsDisplayed);
+        }
+
+        public void RedrawBufferedGraphic()
+        {
+            this.bufferedGraphic.IsDirty = true;
+        }
+
+        /// <summary>
+        /// Implement your painting in this method
+        /// </summary>
+        protected virtual void Draw(Graphics gr)
+        {
+            gr.Clear(Color.White);
+
+            // Display a list of first 100 symbols
+            Font font = new Font("Arial", 10, FontStyle.Regular);
+            Brush brush = new SolidBrush(this.Color);
+            var symbols = Core.Instance.Symbols.Take(100);
+            int currentY = 10;
+            foreach (var symbol in symbols)
+            {
+                gr.DrawString(symbol.Name, font, brush, 20, currentY);
+                gr.DrawString(symbol.SymbolType.ToString(), font, brush, 200, currentY);
+                gr.DrawString(symbol.Connection.VendorName.ToString(), font, brush, 400, currentY);
+
+                currentY += 20;
+            }
+        }
+
+        public override object Render() => bufferedGraphic.CurrentImage;
+
+        public override void Dispose()
+        {
+            if (this.bufferedGraphic != null)
+                this.bufferedGraphic.Dispose();
+
+            base.Dispose();
+        }
+
+        public override void OnResize()
+        {
+            base.OnResize();
+
+            Rectangle bounds = this.Bounds;
+            if (bounds.Width == 0 || bounds.Height == 0)
+                return;
+
+            //
+            try
+            {
+                //
+                // Recreate buffer
+                //
+                bufferedGraphic.Resize(bounds.Width, bounds.Height);
+                bufferedGraphic.IsDirty = true;
+            }
+            catch { }
+        }
+
+        #region Mouse Processing
+
+        private void OnMouseDown(NativeMouseEventArgs e)
+        {
+            // Add your MouseDown processing logic here
+        }
+
+        private void OnMouseMove(NativeMouseEventArgs obj)
+        {
+            // Add your MouseMove processing logic here
+        }
+
+        private void OnMouseUp(NativeMouseEventArgs obj)
+        {
+            // Add your MouseUp processing logic here
+        }
+
+        #endregion
+    }
+}
